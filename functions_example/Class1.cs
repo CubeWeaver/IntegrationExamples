c82d60220084ccdc0c2b84a7766d8f185062c314
CubeWeaver <vlapiner@cubeweaver.com>
2020-10-07 12:05:09 +0200

    user defined functions example added

---
+++ functions_example/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace functions_example
+{
+    public static class FunctionLibrary
+    {
+        public static decimal sqsum(IEnumerable<decimal> a)
+        {
+            return a.Sum(x => x * x);
+        }
+
+        public static decimal gcd(decimal x, decimal y)
+        {
+            ulong a = (ulong)x;
+            ulong b = (ulong)y;
+
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a | b;
+        }
+
+        public static IEnumerable<decimal> join_seq(IEnumerable<decimal> a, IEnumerable<decimal> b)
+        {
+            return a.Concat(b);
+        }
+    }
+}
